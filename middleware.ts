99f2ab111b56c9d27a77738c863420d38137a5d9
PhamManhCuong <cuongpham.251203@gmail.com>
2024-10-02 23:35:28 +0700

    fix: :bug: 414: URI_TOO_LONG

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/api/:path*"]);
+const isPublicRoute = createRouteMatcher([
+  "/api/:path*",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+]);
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
